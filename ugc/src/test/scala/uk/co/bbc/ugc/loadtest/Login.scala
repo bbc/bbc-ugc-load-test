a3bc6de96d34666099a0822b20124572b6dc6536
baahk01 <kodjo.baah@bbc.co.uk>
2018-10-18 15:41:27 +0100

    removed passowrd

--- ugc/src/test/scala/uk/co/bbc/ugc/loadtest/Login.scala
+++ ugc/src/test/scala/uk/co/bbc/ugc/loadtest/Login.scala
@@ -22,7 +22,7 @@ object Login {
       .post("https://account.test.bbc.com/signin")
       .header(HttpHeaderNames.ContentType, HttpHeaderValues.ApplicationFormUrlEncoded)
       .formParam("username", "ugc-under13-testuser1")
-      .formParam("password", "test4656")
+      .formParam("password", "xxx")
       .check(status.in(400, 500)))
     .exec(http("get_session_info")
       .get("https://session.test.bbc.co.uk/session"))
